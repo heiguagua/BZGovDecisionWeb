216c4cf4bc92d0cf8163bca9a5605b62b24b0ab9
xulan <xulan@chinawiserv.com>
2016-11-15 18:01:17 +0800

    Dynamic gennerate date dom

--- artifact/partials/module/content/detail/script.js
+++ artifact/partials/module/content/detail/script.js
@@ -6,32 +6,46 @@
     '$scope', 'detailService', '$stateParams',
     function($scope, detailService, $stateParams) {
       var vm = this;
-      getContent($stateParams.pid);
+      $scope.popups = [];
+      detailService.getContent({
+        menuId: $stateParams.pid
+      }).then(function(result) {
+        vm.content = (result && result.data) ? result.data.body : "";
+        _.forEach(vm.content, function(item) {
+          var popup = {};
+          popup.opened = false;
+          popup.model = new Date();
 
-      $scope.dateOptions = {
-        minMode:'month',
-        datepickerMode:'month',
-        formatYear: 'yyyy',
-        startingDay: 1
-      };
+          var dateOptions = {};
+          dateOptions.formatYear = 'yyyy';
+          if(item.queryTime == 'year') {
+            popup.format = 'yyyy';
+            dateOptions.minMode = 'year';
+            dateOptions.datepickerMode = 'year';
+          }
+          if(item.queryTime == 'month') {
+            popup.format = 'yyyy-MM';
+            dateOptions.minMode = 'month';
+            dateOptions.datepickerMode = 'month';
+            popup.model.setMonth(popup.model.getMonth()-1);
+          }
+          popup.dateOptions = dateOptions;
+          $scope.popups.push(popup);
+        });
+      })
+      //
+      // $scope.dateOptions = {
+      //   minMode: 'month',
+      //   datepickerMode: 'month',
+      //   formatYear: 'yyyy',
+      //   startingDay: 1
+      // };
 
-      $scope.open1 = function() {
-        $scope.popup1.opened = true;
+      $scope.open = function(index) {
+        $scope.popups[index].opened = true;
       };
-      $scope.popup1 = {
-        opened: false
-      };
-      $scope.formats = ['yyyy-MM', 'yyyy'];
-      $scope.format = $scope.formats[0];
       $scope.altInputFormats = ['M!/d!/yyyy'];
 
-      function getContent(id) {
-        detailService.getContent({
-          menuId: id
-        }).then(function(result) {
-          vm.content = (result && result.data) ? result.data.body : "";
-        })
-      }
     }
   ]);
 
